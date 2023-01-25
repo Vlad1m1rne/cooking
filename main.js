e38517f7d18b609104ac1ffca779217cc887dd09
vlad1m1rne <vlad1m1rne@mail.ru>
2023-01-25 22:23:51 +0300

    add mainImg

--- main.js
+++ main.js
@@ -1,27 +1,14 @@
-$(document).ready(function(){
-$('.dn').on({
-  "click" : function(){ $('body').toggleClass('night');
-  if($('body').attr('class') == 'night'){
-    $('.dn').attr('src',"./images/day.png");
-    $("li img").attr("src","./images/arrow2.png");
-  }
-  else{ $('.dn').attr('src',"./images/night.png");
-  $("li img").attr("src","./images/arrow1.png");
-}
-}
-
-
-});
-
-
-
-
-
-
-
-
-
-
-
+$(document).ready(function () {
+  $('.dn').click(function () {
+    $('body').toggleClass('night');
+    if ($('body').attr('class') == 'night') {
+      $('.dn').attr('src', "./images/day.png");
+      $("li img").attr("src", "./images/arrow2.png");
+    }
+    else {
+      $('.dn').attr('src', "./images/night.png");
+      $("li img").attr("src", "./images/arrow1.png");
+    }
+  });
 
 });
