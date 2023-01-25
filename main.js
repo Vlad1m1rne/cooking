9ec5a604869a652c3755f99537e3674e818be8ac
vlad1m1rne <vlad1m1rne@mail.ru>
2023-01-25 21:58:41 +0300

    many changes

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
 $(document).ready(function(){
+$('.dn').on({
+  "click" : function(){ $('body').toggleClass('night');
+  if($('body').attr('class') == 'night'){
+    $('.dn').attr('src',"./images/day.png");
+    $("li img").attr("src","./images/arrow2.png");
+  }
+  else{ $('.dn').attr('src',"./images/night.png");
+  $("li img").attr("src","./images/arrow1.png");
+}
+}
 
+
+});
 
 
 
